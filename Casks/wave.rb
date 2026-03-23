a339f05fc1c9c12ad57d004e82f34aabe530a7c2
shkumbinhasani <sh131171@gmail.com>
2026-03-23 04:24:24 +0100

    Update wave to 0.1.16

--- Casks/wave.rb
+++ Casks/wave.rb
@@ -1,6 +1,6 @@
 cask "wave" do
-  version "0.1.15"
-  sha256 "776e46573816337abd9efb0d2ff252d509a25df78880ebeb88161a7cce0bd732"
+  version "0.1.16"
+  sha256 "ba7e0f2f705af0c8a7621f57463310fc44ec2b4c6be778351b28405745f58b2e"
 
   url "https://github.com/shkumbinhasani/wave/releases/download/v#{version}/wave-macos-arm64.zip"
   name "Wave Terminal"
