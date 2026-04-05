24573957832333509fdbe008bf2ad64c449ce16a
shkumbinhasani <sh131171@gmail.com>
2026-04-06 00:36:38 +0200

    Update wave to 0.5.3

--- Casks/wave.rb
+++ Casks/wave.rb
@@ -1,6 +1,6 @@
 cask "wave" do
-  version "0.5.2"
-  sha256 "7fc506d85d70f60ecc40c74a0efbf9448f82facbb991f83de56a39a265abe74e"
+  version "0.5.3"
+  sha256 "7fbca74c64292d8b3bcce32ac879c8e7f8a5010a218b19a8476f07e601c0197b"
 
   url "https://github.com/shkumbinhasani/wave/releases/download/v#{version}/wave-macos-arm64.zip"
   name "Wave Terminal"
